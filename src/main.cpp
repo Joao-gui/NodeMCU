c99dcb8ee5b8999e0d097f93030fc4a51c0e389b
João Guilherme <eng.joaopellacani@gmail.com>
2022-07-25 21:08:42 -0300

    Primeiro commit para salvar no GitHub

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <Arduino.h>
+
+#define PIN_LED 2
+
+/**
+ * @brief stup inícial da aplicação 
+ * 
+ */
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+}
+
+/**
+ * @brief código da aplicação
+ * 
+ */
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
+}
